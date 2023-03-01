eb61af02f0d74559f98a5f289fa590061d732399
Your Name <lajika@MacBook-Air-Jibek.local>
2023-03-01 13:18:04 +0600

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <>APP</>;
+import React from 'react';
+import MainRoutes from './MainRoutes';
+
+const App = () => {
+  return (
+    <div>
+      <MainRoutes />
+    </div>
+  )
 }
 
-export default App;
+export default App

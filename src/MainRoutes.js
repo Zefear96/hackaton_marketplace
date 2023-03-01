8d076101bb40d59e2b02c62a80dfcd107c3a5a9f
Zamir <eazzy_freez@mail.ru>
2023-03-01 13:30:48 +0600

    add nav

--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import {Routes, Route} from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 const MainRoutes = () => {
   return (
     <Routes>
-        <Route path='' element={<></>}/>
+      <Route path="" element={<></>} />
     </Routes>
-  )
-}
+  );
+};
 
-export default MainRoutes
+export default MainRoutes;

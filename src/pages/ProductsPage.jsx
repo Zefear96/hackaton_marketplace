1e96f8c3b08ebd9acb4a83f1ee8bfe6b599d6fed
Your Name <lajika@MacBook-Air-Jibek.local>
2023-03-02 11:54:13 +0600

    filter+slidebar

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductList from "../components/products/ProductsList/ProductsList";
+import ProductSideBar from "../components/products/ProductSideBar/ProductSideBar";
 
 const ProductsPage = () => {
-  return <ProductList />;
+
+  const [isSideBar, setIsSideBar] = useState(true);
+  const [page, setPage] = useState(1);
+
+  function changeSideBarStatus() {
+    setIsSideBar(!isSideBar)
+  };
+
+  return <>
+  <ProductList changeSideBarStatus={changeSideBarStatus} page={page} setPage={setPage}/>
+  <ProductSideBar isSideBar={isSideBar} setPage={setPage}/>
+  </>;
 };
 
 export default ProductsPage;

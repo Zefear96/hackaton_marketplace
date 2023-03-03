7b292353529e056c1f81dee8fecc277d714a7880
Your Name <lajika@MacBook-Air-Jibek.local>
2023-03-03 17:22:13 +0600

    login-registration

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from 'react';
+// import { useAuth } from '../contexts/AuthContextProvider'; //свой путь
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
-};
 
-export default LoginPage;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { login, error } = useAuth(); //вытаскивает контекст
+
+  return (
+    <div style={style}>
+
+      {error ? (
+        <h3>{error}</h3>
+      ) : (
+        ''
+      )}
+
+      <input type="text" placeholder='username' onChange={e => setUsername(e.target.value)} />
+      <input type="text" placeholder='password' onChange={e => setPassword(e.target.value)} />
+      <button onClick={() => login(username, password)}>Login</button>
+    </div>
+  )
+}
+
+export default LoginPage

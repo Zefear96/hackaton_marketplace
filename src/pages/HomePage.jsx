f733142ebbb8be4a5ec6c02eacf0372ea89ab4ff
Zamir <eazzy_freez@mail.ru>
2023-03-06 15:30:10 +0600

    add styles home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -52,19 +52,46 @@ const HomePage = () => {
           />
         </video>
       </div>
-      <div className="promo">
+      <div className="promo-block">
+        <div className="promo-video">
+          <video loop autoPlay muted>
+            <source
+              src="https://www.glenfiddich.com/gf_s3_assets/gf_drupal_assets/videos/GLEN_Website_PDP-Video-15YO_Home.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+        <div className="promo-center">
+          <img
+            src="https://www.glenfiddich.com/sites/default/files/2022-04/15yo-single-malt-scotch-whisky.png"
+            alt=""
+          />
+        </div>
+        <div className="promo-text">
+          <h2>DRINK WELL. DO GOOD.</h2>
+          <p>
+            Welcome to our online store for premium alcoholic beverages! Indulge
+            yourself in our wide selection of top-quality wines, spirits, and
+            liqueurs from all around the world. From the finest single malt
+            scotch to the rarest vintage champagne, we have everything you need
+            to elevate your drinking experience. Our expertly curated collection
+            includes exclusive and hard-to-find bottles that will satisfy even
+            the most discerning palate. We pride ourselves on providing
+            exceptional customer service, ensuring that your shopping experience
+            is easy and enjoyable.
+          </p>
+        </div>
+      </div>
+
+      <div className="about">
         <video loop autoPlay muted>
           <source
-            src="https://www.glenfiddich.com/gf_s3_assets/gf_drupal_assets/videos/GLEN_Website_PDP-Video-15YO_Home.mp4"
+            src="https://www.glenfiddich.com/gf_s3_assets/gf_drupal_assets/videos/1-GLE-header-visit-the-distillery_1.mp4"
             type="video/mp4"
           />
         </video>
-        <div className="promo-text">
-          <h1>Lorem ipsum dolor sit amet.</h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus dicta voluptas obcaecati! Consequatur et dolores, quam
-          totam natus voluptatem commodi obcaecati repellat, nisi quisquam, quae
-          dicta? Recusandae fugit iusto repudiandae.
+        <div className="about-text">
+          <h2>WELCOME TO OUR HOME</h2>
         </div>
       </div>
     </div>

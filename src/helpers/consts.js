79f705fef71139beb88dd756f0ecd2d109a52eaa
Jika1995 <121999897+Jika1995@users.noreply.github.com>
2023-08-28 19:31:53 +0600

    Update consts.js

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,4 +1,4 @@
-export const JSON_API_PRODUCTS = "http://localhost:8000/products";
+export const JSON_API_PRODUCTS = "https://db-json-for-marketplace-project.vercel.app/products";
 
 export const ACTIONS = {
   GET_PRODUCTS: "GET_PRODUCTS",
